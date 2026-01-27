92dd37cfc18b096cf1b73746cc69db637093366c
fypovich <fypovich@gmail.com>
2026-01-27 10:48:23 +0300

    docker

--- backend/app/core/celery_app.py
+++ backend/app/core/celery_app.py
@@ -4,9 +4,15 @@ from app.core.config import settings
 celery_app = Celery(
     "worker",
     broker=settings.CELERY_BROKER_URL,
-    backend=settings.CELERY_RESULT_BACKEND
+    backend=settings.CELERY_RESULT_BACKEND,
+    # ВАЖНО: Мы явно говорим Celery загрузить модуль с задачами
+    include=["app.worker"]
 )
 
-celery_app.conf.task_routes = {
-    "app.worker.process_meme_task": "main-queue"
-}
+celery_app.conf.update(
+    task_serializer="json",
+    accept_content=["json"],
+    result_serializer="json",
+    timezone="UTC",
+    enable_utc=True,
+)
