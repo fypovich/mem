76dfdbc59386a9be933acfa742bd7a91abe323b4
fypovich <fypovich@gmail.com>
2026-01-29 13:27:29 +0300

    optimization

--- backend/app/core/celery_app.py
+++ backend/app/core/celery_app.py
@@ -1,20 +1,19 @@
 from celery import Celery
-from celery.schedules import crontab
 from app.core.config import settings
 
 celery_app = Celery("worker", broker=settings.CELERY_BROKER_URL)
 
 celery_app.conf.update(
-    task_routes={
-        "app.worker.process_meme_task": "main-queue",
-        "app.worker.index_meme_task": "main-queue",
-        "app.worker.delete_index_task": "main-queue",
-        "app.worker.sync_views_task": "main-queue",
-    },
+    # ВАЖНО: Явно указываем, где лежат наши задачи, чтобы Celery их нашел
+    imports=['app.worker'],
+    
+    # УБРАЛИ task_routes: теперь все задачи идут в стандартную очередь 'celery',
+    # которую воркер слушает по умолчанию.
+    
     beat_schedule={
         "sync-views-every-30-seconds": {
             "task": "app.worker.sync_views_task",
-            "schedule": 30.0, # Каждые 30 секунд
+            "schedule": 30.0, # Запуск каждые 30 секунд
         },
     },
     timezone="UTC"
